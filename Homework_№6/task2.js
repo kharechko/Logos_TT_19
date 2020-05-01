68438e4cdd587b557752c8f5951b6d55f215056f
kharechko <63398664+kharechko@users.noreply.github.com>
2020-05-01 18:03:20 +0300

    Update task2.js

--- Homework_№6/task2.js
+++ Homework_№6/task2.js
@@ -1,14 +1,17 @@
 
-// function getGeo(number, mult){
-//     for ( let i=1, step1=1,result=1, suma=0; i<=number; i++, step1=step1*mult, result=step1){
-//        document.write(result);
-//      }
-//     }
-//     getGeo(5, 2)
+let getGeo=function(number, mult){
+        let sum=0;
+         let result=1;
+      for ( let i=1, step1=1, suma=0; i<=number; i++, step1=step1*mult, result=step1){
+        sum+=result; 
+     }
+         console.log(sum)
+    }       
+    getGeo(4, 3)
 
-let getGeo=function(n, q){
+// let getGeo=function(n, q){
 
-    let suma=(1-(q**n))/(1-q);
-    return console.log(suma)
-}
-getGeo(4,3)
+//     let suma=(1-(q**n))/(1-q);
+//     return console.log(suma)
+// }
+// getGeo(4,3)
