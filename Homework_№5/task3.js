5cd6c5b29702b91d1988e12639ca889d69b98111
kharechko <63398664+kharechko@users.noreply.github.com>
2020-05-01 11:39:11 +0300

    Update task3.js

--- Homework_№5/task3.js
+++ Homework_№5/task3.js
@@ -1,3 +1,13 @@
-function getMax(){
-    console.log(Math.max(arguments.length))
+// function getMax(){
+//     console.log(Math.max(arguments.length))
+// }
+function maxIn(){
+    let max=-111111;
+     for (let i=0; i<arguments.length; i++){
+         if (arguments[i]>max){
+             max=arguments[i];
+         }
+     }
+     return console.log(max)
 }
+maxIn(1, 386, 5,9,2, 34)
