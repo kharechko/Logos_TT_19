6c4bae2c7f3774562f8194b75a25da2871d305f6
kharechko <63398664+kharechko@users.noreply.github.com>
2020-05-03 06:31:36 +0300

    Update task2.js

--- Homework_№5/task2.js
+++ Homework_№5/task2.js
@@ -1,1 +1,10 @@
 
+function isPrime(num) {
+    if(num < 2) return false;
+    if(num == 2) return console.log(num);
+    for(var i = 2; i < num; i++) {
+      if(num % i === 0) return false;
+    }
+    return console.log(num);
+  } 
+  isPrime(56)
