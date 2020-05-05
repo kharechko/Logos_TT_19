a0b0103bdffaed03fc1e7a1dd0c39f47faa26bce
kharechko <63398664+kharechko@users.noreply.github.com>
2020-05-05 13:35:48 +0300

    Update task3.js

--- Homework_№6/task3.js
+++ Homework_№6/task3.js
@@ -1,4 +1,22 @@
 
+// let number =+prompt('enter the first value please');
+// let num2=+prompt('enter the second value please');
+// function arePrimes(num1, num2){
+// for(let i=num1; i<=num2; i++){
+//     let devisableCount = 2;
+//         for(let x=0; x<=i/2; x++){
+//             if(i !== 1 && i !== 0 && i !== x){
+//                 if(i%x == 0){
+//                    devisableCount++;
+//                  }
+//             }
+//         }
+//     if(devisableCount == 3){
+//         console.log(i);
+//     }
+// }
+// }
+// arePrimes(number, num2)
 let arePrime=(start, end)=>{
 for (let i=start; i<=end; i++){
     let count =0;
