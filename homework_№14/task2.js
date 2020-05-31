cf3b746c5acc9dbe359d22bee503d9fcb108c17b
undefined <63398664+kharechko@users.noreply.github.com>
2020-05-31 10:53:16 +0300

    Update task2.js

--- homework_№14/task2.js
+++ homework_№14/task2.js
@@ -1,10 +1,6 @@
 
-
 let getSel = sel => document.querySelector(sel);
-
-
 let newForm = document.forms['account'];
-
 let userBio = getSel('.second_page');
 userBio.classList.remove('second_page');
 userBio.classList.add('non-visible');
@@ -19,37 +15,24 @@ let userJob = getSel('.user_job');
 let bottomBtn = getSel('.bottom_btn');
 let sel = getSel('.sel');
 
-
-
-
-
-
-
-
 newForm.sign_up.addEventListener('click', function() {
-
   if(newForm.check.checked) {
      newForm.classList.add('non-visible');
      userBio.classList.add('second_page');
-       if(userMan.checked) userIcon.classList.add('portrait_man');
-       else if(userWoman.checked) userIcon.classList.add('portrait_woman'); 
+       if(userMan.checked == false ) userIcon.classList.add('portrait_woman');
+       else if(userWoman.checked  == false) userIcon.classList.add('portrait_man'); 
           userBim.innerText = newForm.f_name.value + ' ' + newForm.s_name.value;
           userAddress.innerText = newForm.address.value;
           userJob.innerText =  newForm.user_job.options[newForm.user_job.selectedIndex].value;
-
-        }
-
-     })
-
-
-
-
+          }
+       })
 
 bottomBtn.addEventListener('click', function(){
   newForm.classList.remove('non-visible');
+  userIcon.classList.remove('portrait_woman');
+  userIcon.classList.remove('portrait_man');
   userBio.classList.remove('second_page');
   userBio.classList.add('non-visible');
   newForm.reset();
-
+ 
 })
-
