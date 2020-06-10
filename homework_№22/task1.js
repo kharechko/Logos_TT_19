904a532aa389958d886d570ff6fcf55a751794f4
undefined <63398664+kharechko@users.noreply.github.com>
2020-06-10 19:55:42 +0300

    Add files via upload

---
+++ homework_№22/task1.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    
+        $('.element').on('click',function(){
+            $('.element').addClass('s1', 1000).animate({
+            width: Math.floor(Math.random() * 400 + 10) +'px',
+            height: Math.floor(Math.random() * 400 + 10) +'px',
+            top: Math.floor(Math.random()*400) + 'px',
+            left: Math.floor(Math.random()*800) +'px',
+            backgroundColor: "#"+((1<<24)*Math.random()|0).toString(16),   
+
+    }, 1000, 'easeInOutBack')})
+})
+
