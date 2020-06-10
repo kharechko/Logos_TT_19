6b8d4e51fc3758422cd37fc50e1a19c76d3866f3
undefined <63398664+kharechko@users.noreply.github.com>
2020-06-10 19:57:53 +0300

    Update task2.js

--- homework_№22/task2.js
+++ homework_№22/task2.js
@@ -1,7 +1,4 @@
-
-
 let borderStyle = ['dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset', 'none'];
-
 function randomArr(arr){
     for (let i = arr.length - 1; i > 0; i--) {
         let arr2 = Math.floor(Math.random() * (i + 1));
@@ -9,25 +6,21 @@ function randomArr(arr){
       }
    return arr
 }
-
-
 $(document).ready(function() {
- let getDisco = function() {
+ let goDisco = function() {
     let bdCol = "#"+((1<<24)*Math.random()|0).toString(16);
     let borderRandomWidth =  Math.floor(Math.random()*10)
     $('.ball').css({
         boxShadow: '0px 0px 60px '  + bdCol,
         border: borderRandomWidth + 'px ' +   randomArr(borderStyle[Math.floor(Math.random()*9)]) +   " #"+((1<<24)*Math.random()|0).toString(16),
     }).animate({
-                width: Math.floor(Math.random() * 400 + 10) +'px',
-                height: Math.floor(Math.random() * 400 + 10) +'px',
-                top: Math.floor(Math.random()*400) + 'px',
-                left: Math.floor(Math.random()*800) +'px',
-                backgroundColor: bdCol,
-       }, 500, 'linear')}
-setInterval(getDisco, 800)
-
-
+       width: Math.floor(Math.random() * 400 + 10) +'px',
+       height: Math.floor(Math.random() * 400 + 10) +'px',
+       top: Math.floor(Math.random()*400) + 'px',
+       left: Math.floor(Math.random()*800) +'px',
+       backgroundColor: bdCol,
+ }, 500, 'linear')}
+ setInterval(goDisco, 800)
 })
 
 
