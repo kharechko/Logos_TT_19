4e3769887e9e33ecdeb9fa3de28711c09bb21464
undefined <63398664+kharechko@users.noreply.github.com>
2020-06-10 21:19:02 +0300

    Update task1.js

--- homework_№22/task1.js
+++ homework_№22/task1.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    
         $('.element').on('click',function(){
             $('.element').animate({
             width: Math.floor(Math.random() * 400 + 10) +'px',
@@ -7,7 +6,6 @@ $(document).ready(function () {
             top: Math.floor(Math.random()*400) + 'px',
             left: Math.floor(Math.random()*800) +'px',
             backgroundColor: "#"+((1<<24)*Math.random()|0).toString(16),   
-
     }, 1000, 'easeInOutBack')})
 })
 
