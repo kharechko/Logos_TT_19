94df4f106d8818b9505bc3bfc7bc50c525048894
undefined <63398664+kharechko@users.noreply.github.com>
2020-06-11 17:37:47 +0300

    Add files via upload

---
+++ homework_№23/task3.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+   function getAnimate(){
+       $('.move').animate({
+        height: '0px',   
+      }, 60000).delay(4000).animate({
+        height: '550px',
+  },  60000)
+}
+   
+function updateCountDown() {
+ let count = 2;
+    txt =  setInterval (function() {
+    count--
+   $('.count').text(count)
+   if(count < 0) clearInterval(txt)
+  }, 60000)  
+}
+  setTimeout(getAnimate, 100) 
+  setTimeout(updateCountDown, 100)      
+});
+
+
+
+
